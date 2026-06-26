ed471ade6f492728bdbc77afca201580464fa287
laurenseperac-blip <laurenseperac@gmail.com>
2026-06-26 12:13:13 -0400

    modify server port

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + "/public"));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`))
